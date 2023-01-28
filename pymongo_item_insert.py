7126618b3657ae93d673bd6d311a2527f685a1b3
youngjun-yoo16 <yoo156@purdue.edu>
2023-01-28 14:00:33 +0900

    Added Jacket 1 to DB

--- pymongo_item_insert.py
+++ pymongo_item_insert.py
@@ -4,3 +4,14 @@ def add_items():
     
     dbname = get_database()
     collection_name = dbname["jacket_items"]
+    
+    item_1 = {
+        "name" : "레이나 트위드자켓",
+        "fit" : "레귤러 핏",
+        "length" : "기본 기장",
+        "neck_line" : "높은 넥라인",
+        "design" : "단조로움",
+        "material" : "트위드"
+    }
+    
+    collection_name.insert_one(item_1)
