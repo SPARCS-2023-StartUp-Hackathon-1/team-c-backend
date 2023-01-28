076f1d2a2a7e510161df6fff261048263b7429e7
youngjun-yoo16 <yoo156@purdue.edu>
2023-01-28 14:17:11 +0900

    Added 4 more jacket data

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
   
 def musinsa_crawling():
+    
     count = 0
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/104.0.0.0 Safari/537.36'}
--- pymongo_item_insert.py
+++ pymongo_item_insert.py
@@ -14,4 +14,40 @@ def add_items():
         "material" : "트위드"
     }
     
-    collection_name.insert_one(item_1)
+    item_2 = {
+        "name" : "엔니드 카라 트위드 자켓",
+        "fit" : "레귤러 핏",
+        "length" : "기본 기장",
+        "neck_line" : "보통 넥라인",
+        "design" : "무지",
+        "material" : "트위드"
+    }
+
+    item_3 = {
+        "name" : "Moring jacket (말차)",
+        "fit" : "오버 핏",
+        "length" : "롱 기장",
+        "neck_line" : "파인 넥라인",
+        "design" : "무지",
+        "material" : "울"
+    }
+    
+    item_4 = {
+        "name" : "도넬 세미크롭 자켓",
+        "fit" : "레귤러 핏",
+        "length" : "기본 기장",
+        "neck_line" : "파인 넥라인",
+        "design" : "무지",
+        "material" : "폴리에스터"
+    }
+    
+    item_5 = {
+        "name" : "루닝 라운드 트위드 자켓",
+        "fit" : "레귤러 핏",
+        "length" : "크롭 기장",
+        "neck_line" : "보통 넥라인",
+        "design" : "단조로움",
+        "material" : "트위드"
+    }
+    
+    collection_name.insert_many([item_2, item_3, item_4, item_5])
