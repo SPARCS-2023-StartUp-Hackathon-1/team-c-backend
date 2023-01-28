450220b4fb18b7122e15820db8acec0c32899cc1
youngjun-yoo16 <yoo156@purdue.edu>
2023-01-29 03:08:17 +0900

    Finished bottom logic and optimized top logic

--- bottom_logic.py
+++ bottom_logic.py
@@ -1,4 +1,4 @@
-from db.initialize import get_database
+from db_initialize import get_database
 
 def calc_score(product_name: str):
     
@@ -9,30 +9,30 @@ def calc_score(product_name: str):
     
     denim_pants = collection_name.find()
     for pant in denim_pants:
-        if pant.name == product_name:
-            if pant.fit == "슬림" or pant.fit == "루즈":
+        if pant['name'] == product_name:
+            if pant['fit'] == "슬림" or pant['fit'] == "루즈":
                 total+=10
-            elif pant.fit == "레귤러":
+            elif pant['fit'] == "레귤러":
                 total+=20
-            if pant.texture == "약간 부드러움" or pant.texture == "약간 뻣뻣함":
+            if pant['texture'] == "약간 부드러움" or pant['texture'] == "약간 뻣뻣함":
                 total+=10
-            elif pant.texture == "보통":
+            elif pant['texture'] == "보통":
                 total+=20
-            if pant.stretchiness == "거의 없음" or pant.stretchiness == "약간 있음":
+            if pant['stretchiness'] == "거의 없음" or pant['stretchiness'] == "약간 있음":
                 total+=10
-            elif pant.stretchiness == "보통":
+            elif pant['stretchiness'] == "보통":
                 total+=20
-            if pant.transparency == "보통":
+            if pant['transparency'] == "보통":
                 total+=10
-            elif pant.transparency == "거의 없음" or pant.transparency == "없음":
+            elif pant['transparency'] == "거의 없음" or pant['transparency'] == "없음":
                 total+=20
-            if pant.thickness == "약간 얇음" or pant.thickness == "약간 두꺼움":
+            if pant['thickness'] == "약간 얇음" or pant['thickness'] == "약간 두꺼움":
                 total+=10
-            elif pant.thickness == "보통":
+            elif pant['thickness'] == "보통":
                 total+=20
                 
     print(str(total) + "%")
 
 if __name__=="__main__":
-    product_name = str(input("자켓 상품명을 입력하세요: "))
+    product_name = str(input("데님바지 상품명을 입력하세요: "))
     calc_score(product_name)
--- top_logic.py
+++ top_logic.py
@@ -10,29 +10,19 @@ def calc_score(product_name: str):
     jackets = collection_name.find()
     for jacket in jackets:
         if jacket['name'] == product_name:
-            if jacket['fit'] == "슬림 핏" or jacket['fit'] == "오버 핏":
-                total+=0
-            elif jacket['fit'] == "레귤러 핏":
+            if jacket['fit'] == "레귤러 핏":
                 total+=20
-            if jacket['length'] == "크롭 기장" or jacket['length'] == '롱 기장':
-                total+=0
-            elif jacket['length'] == "기본 기장":
+            if jacket['length'] == "기본 기장":
                 total+=20
-            if jacket['neck_line'] == "높은 넥라인":
-                total+=0
-            elif jacket['neck_line'] == "보통 넥라인":
+            if jacket['neck_line'] == "보통 넥라인":
                 total+=10
             elif jacket['neck_line'] == "파인 넥라인":
                 total+=20
-            if jacket['design'] == "화려함":
-                total+=0
-            elif jacket['design'] == "단조로움":
+            if jacket['design'] == "단조로움":
                 total+=10
             elif jacket['design'] == "무지":
                 total+=20
-            if jacket['material'] == "트위드":
-                total+=0
-            elif jacket['material'] == "폴리에스터" or jacket['material'] == "스웨이드":
+            if jacket['material'] == "폴리에스터" or jacket['material'] == "스웨이드":
                 total+=10
             elif jacket['material'] == "울" or jacket['material'] == "캐새미어" or jacket['material'] == "천연가죽" or jacket['material'] == "코튼" or jacket['material'] == "데님":
                 total+=20   
