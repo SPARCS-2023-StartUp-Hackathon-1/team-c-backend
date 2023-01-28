8a97e75a0a3fbc7ae8be482cf50d7b69558038f5
youngjun-yoo16 <yoo156@purdue.edu>
2023-01-28 22:42:02 +0900

    Added all 9 items with their respective S3 URL addrress

--- pymongo_get_database.py
+++ pymongo_get_database.py
@@ -1,10 +1,13 @@
 from pymongo import MongoClient
+import certifi
 
 def get_database():
     
+    ca = certifi.where()
+    
     CONNECTION_STRING = "mongodb+srv://jun916:110958yjy@db-cluster.bkfftpf.mongodb.net/?retryWrites=true&w=majority"
  
-    client = MongoClient(CONNECTION_STRING)
+    client = MongoClient(CONNECTION_STRING, tlsCAFile=ca)
  
     return client['clothes_details']
 
--- pymongo_item_insert.py
+++ pymongo_item_insert.py
@@ -1,25 +1,9 @@
 from pymongo_get_database import get_database
-from bson import ObjectId
-from PIL import Image
-import io
-
-def insert_image():
-
-    im = Image.open("img/item_1/image.png")
-    
-    image_bytes = io.BytesIO()
-    im.save(image_bytes, format='PNG')
 
 def add_items():
     
     dbname = get_database()
     collection_name = dbname["jacket_items"]
-    
-    im1 = Image.open("img/item_1/image1.png")
-    
-    item_1_image_bytes = io.BytesIO()
-    im1.save(item_1_image_bytes, format='PNG')
-    item_1_value = item_1_image_bytes.getvalue()
     
     item_1 = {
         "name" : "레이나 트위드자켓",
@@ -29,10 +13,17 @@ def add_items():
         "design" : "단조로움",
         "material" : "트위드",
         "price" : 73520,
-        "image_data" : item_1_value
+        "image1_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_1/image1.png",
+        "image1_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_1/image1.png",
+        "image2_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_1/image2.jpg",
+        "image2 URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_1/image2.jpg",
+        "image3_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_1/image3.jpg",
+        "image3_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_1/image3.jpg",
+        "image4_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_1/image4.jpg",
+        "image4_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_1/image4.jpg"
     }
     
-    """item_2 = {
+    item_2 = {
         "name" : "엔니드 카라 트위드 자켓",
         "fit" : "레귤러 핏",
         "length" : "기본 기장",
@@ -40,7 +31,14 @@ def add_items():
         "design" : "무지",
         "material" : "트위드",
         "price" : 56950,
-        "image_data" :
+        "image1_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_2/image1.png",
+        "image1_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_2/image1.png",
+        "image2_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_2/image2.jpg",
+        "image2_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_2/image2.jpg",
+        "image3_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_2/image3.jpg",
+        "image3_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_2/image3.jpg",
+        "image4_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_2/image4.jpg",
+        "image4_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_2/image4.jpg"
     }
 
     item_3 = {
@@ -51,7 +49,14 @@ def add_items():
         "design" : "무지",
         "material" : "울",
         "price" : 86700,
-        "image_data" :
+        "image1_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_3/image1.png",
+        "image1_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_3/image1.png",
+        "image2_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_3/image2.jpg",
+        "image2_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_3/image2.jpg",
+        "image3_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_3/image3.jpg",
+        "image3_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_3/image3.jpg",
+        "image4_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_3/image4.jpg",
+        "image4_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_3/image4.jpg"
     }
     
     item_4 = {
@@ -62,7 +67,14 @@ def add_items():
         "design" : "무지",
         "material" : "폴리에스터",
         "price" : 46430,
-        "image_data" :
+        "image1_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_4/image1.png",
+        "image1_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_4/image1.png",
+        "image2_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_4/image2.jpg",
+        "image2_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_4/image2.jpg",
+        "image3_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_4/image3.jpg",
+        "image3_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_4/image3.jpg",
+        "image4_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_4/image4.jpg",
+        "image4_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_4/image4.jpg"
     }
     
     item_5 = {
@@ -73,10 +85,69 @@ def add_items():
         "design" : "단조로움",
         "material" : "트위드",
         "price" : 32400,
-        "image_data" :
-    }"""
+        "image1_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_5/image1.png",
+        "image1_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_5/image1.png",
+        "image2_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_5/image2.jpg",
+        "image2_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_5/image2.jpg",
+        "image3_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_5/image3.jpg",
+        "image3_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_5/image3.jpg",
+        "image4_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_5/image4.jpg",
+        "image4_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_5/image4.jpg"
+    }
+        
+    item_6 = {
+        "name" : "WOMEN 크롭 오버핏 마일드레더 블레이저 가죽자켓",
+        "price" : 71910,
+        "image1_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_6/image1.jpg",
+        "image1_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_6/image1.jpg",
+        "image2_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_6/image2.jpg",
+        "image2_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_6/image2.jpg",
+        "image3_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_6/image3.jpg",
+        "image3_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_6/image3.jpg",
+        "image4_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_6/image4.jpg",
+        "image4_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_6/image4.jpg"
+    }
     
-    collection_name.insert_one(item_1)
+    item_7 =  {
+        "name" : "아로 크롭 숏 블레이저 브이넥 무지 노카라 자켓",
+        "price" : 44100,
+        "image1_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_7/image1.jpg",
+        "image1_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_7/image1.jpg",
+        "image2_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_7/image2.jpg",
+        "image2_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_7/image2.jpg",
+        "image3_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_7/image3.jpg",
+        "image3_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_7/image3.jpg",
+        "image4_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_7/image4.jpg",
+        "image4_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_7/image4.jpg"
+    }
+    
+    item_8 = {
+        "name" : "세미 크롭 원버튼 자켓 블레이져",
+        "price" : 39330,
+        "image1_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_8/image1.jpg",
+        "image1_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_8/image1.jpg",
+        "image2_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_8/image2.jpg",
+        "image2_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_8/image2.jpg",
+        "image3_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_8/image3.jpg",
+        "image3_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_8/image3.jpg",
+        "image4_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_8/image4.jpg",
+        "image4_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_8/image4.jpg"
+    }
+    
+    item_9 = {
+        "name" : "고퀄리티 런던 패드 금장버튼 트위드 숏 여자 자켓 가을코디 간절기룩",
+        "price" : 133870,
+        "image1_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_9/image1.jpg",
+        "image1_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_9/image1.jpg",
+        "image2_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_9/image2.jpg",
+        "image2_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_9/image2.jpg",
+        "image3_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_9/image3.jpg",
+        "image3_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_9/image3.jpg",
+        "image4_URL" : "https://sparcs-2023-startup-hackathon-c-1.s3.ap-northeast-2.amazonaws.com/item_9/image4.jpg",
+        "image4_URI" : "s3://sparcs-2023-startup-hackathon-c-1/item_9/image4.jpg"
+    }
+    
+    collection_name.insert_many([item_1, item_2, item_3, item_4, item_5, item_6, item_7, item_8, item_9])
     
 if __name__ == "__main__":   
     add_items()
