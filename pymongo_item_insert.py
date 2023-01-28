ad9d11afbc44ba148e4f179b45b9a94bd313b564
youngjun-yoo16 <yoo156@purdue.edu>
2023-01-28 17:30:05 +0900

    Added 3 more images each for first 4 items

--- pymongo_item_insert.py
+++ pymongo_item_insert.py
@@ -1,17 +1,33 @@
 from pymongo_get_database import get_database
+from bson import ObjectId
+from PIL import Image
+import io
+
+def insert_image():
+
+    im = Image.open("img/item_1/image.png")
+    
+    image_bytes = io.BytesIO()
+    im.save(image_bytes, format='PNG')
 
 def add_items():
     
     dbname = get_database()
     collection_name = dbname["jacket_items"]
     
-    item_1 = {
+    im1 = Image.open("img/item_1/image.png")
+    
+    item_1_image_bytes = io.BytesIO()
+    im1.save(item_1_image_bytes, format='PNG')
+    
+    """item_1 = {
         "name" : "레이나 트위드자켓",
         "fit" : "레귤러 핏",
         "length" : "기본 기장",
         "neck_line" : "높은 넥라인",
         "design" : "단조로움",
-        "material" : "트위드"
+        "material" : "트위드",
+        "image_data" : item_1_image_bytes.getvalue()
     }
     
     item_2 = {
@@ -20,7 +36,8 @@ def add_items():
         "length" : "기본 기장",
         "neck_line" : "보통 넥라인",
         "design" : "무지",
-        "material" : "트위드"
+        "material" : "트위드",
+        "image_data" :
     }
 
     item_3 = {
@@ -29,7 +46,8 @@ def add_items():
         "length" : "롱 기장",
         "neck_line" : "파인 넥라인",
         "design" : "무지",
-        "material" : "울"
+        "material" : "울",
+        "image_data" :
     }
     
     item_4 = {
@@ -38,7 +56,8 @@ def add_items():
         "length" : "기본 기장",
         "neck_line" : "파인 넥라인",
         "design" : "무지",
-        "material" : "폴리에스터"
+        "material" : "폴리에스터",
+        "image_data" :
     }
     
     item_5 = {
@@ -47,10 +66,11 @@ def add_items():
         "length" : "크롭 기장",
         "neck_line" : "보통 넥라인",
         "design" : "단조로움",
-        "material" : "트위드"
+        "material" : "트위드",
+        "image_data" :
     }
     
-    collection_name.insert_many([item_1, item_2, item_3, item_4, item_5])
+    collection_name.insert_many([item_1, item_2, item_3, item_4, item_5])"""
     
 if __name__ == "__main__":   
     add_items()
